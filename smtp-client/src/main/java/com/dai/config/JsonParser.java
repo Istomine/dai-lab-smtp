f1d2526692768987b7d5735886739264c2363293
Delay Benoit <benoit.delay@heig-vd.ch>
2023-11-30 13:29:56 +0100

    Implemented message parser

--- smtp-client/src/main/java/com/dai/config/JsonParser.java
+++ smtp-client/src/main/java/com/dai/config/JsonParser.java
@@ -1,4 +1,25 @@
 package com.dai.config;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.readString;
+
 public class JsonParser {
+    private String fileContent;
+
+    public JsonParser(String path){
+        try {
+            fileContent = readString(Paths.get(path));
+        }catch (IOException e){
+            System.out.println("Error while reading file : " + e.toString());
+        }
+    }
+
+    public JSONObject loadJsonObject(){
+        return new JSONObject(fileContent);
+    }
 }
--- smtp-client/src/main/java/com/dai/config/Message.java
+++ smtp-client/src/main/java/com/dai/config/Message.java
@@ -1,4 +1,39 @@
 package com.dai.config;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Message {
+    /**
+     *
+     */
+    JSONObject jsonObject;
+
+    /**
+     *
+     * @param pathFile
+     */
+    public Message(String pathFile){
+        JsonParser jsonParser = new JsonParser(pathFile);
+        jsonObject = jsonParser.loadJsonObject();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Mail> getMails(){
+        ArrayList<Mail> messages = new ArrayList<Mail>();
+        JSONArray emails = jsonObject.getJSONArray("emails");
+        JSONObject mail;
+        Mail message;
+        for(int i = 0; i < emails.length(); i++){
+            mail = emails.getJSONObject(i);
+            message = new Mail(mail.get("subject").toString(), mail.get("body").toString());
+            messages.add(message);
+        }
+        return messages;
+    }
 }
