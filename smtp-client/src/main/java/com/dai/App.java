e0f751b727b536fd807d61a7fe7089baaf0c81bf
Istomine <swagman@swag.com>
2023-11-30 14:35:34 +0100

    main advance

--- smtp-client/src/main/java/com/dai/App.java
+++ smtp-client/src/main/java/com/dai/App.java
@@ -1,7 +1,14 @@
 package com.dai;
 
+import com.dai.config.Address;
 import com.dai.config.JsonParser;
+import com.dai.config.Mail;
 import com.dai.config.Message;
+import com.dai.SmtpClient.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Hello world!
@@ -9,7 +16,26 @@ import com.dai.config.Message;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
+
+        Address addr = new Address("./smtp-client/resources/email.json");
+
+        Message mess = new Message("./smtp-client/resources/message.json");
+
+
+        ArrayList<String> mails = addr.getEmailsAddress();
+        ArrayList<Mail> messages = mess.getMails();
+
+        SMTPClient client = new SMTPClient(mails.get(0),mails,"localhost",1025 );
+
+
+        for(Mail m : messages){
+            client.send(m.subject(),m.body());
+        }
+
+
+
+
+
     }
 }
