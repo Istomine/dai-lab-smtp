ec75ed537875693330e0839fc42f76c19517ab75
Istomine <swagman@swag.com>
2023-12-03 15:10:43 +0100

    fonctionnement des envois de message

--- smtp-client/src/main/java/com/dai/App.java
+++ smtp-client/src/main/java/com/dai/App.java
@@ -1,50 +1,53 @@
 package com.dai;
 
 import com.dai.config.Address;
-import com.dai.config.JsonParser;
+import com.dai.GroupManager.*;
 import com.dai.config.Mail;
 import com.dai.config.Message;
 import com.dai.SmtpClient.*;
-
-import javax.management.BadAttributeValueExpException;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
-import java.util.ArrayList;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
 
     public static void main( String[] args ){
 
-        if(args.length != 3){
-            throw new IllegalArgumentException("Le programme nécessite exactement 4 arguments.");
+        try{
+            if(args.length != 3){
+                throw new IllegalArgumentException("The program must have 3 arguments");
+            }
+
+            int groupNumber = Integer.parseInt(args[2]);
+
+            if(groupNumber < 1){
+                throw new IllegalArgumentException("The number of groups must be bigger than 0");
+            }
+
+            // Email and group managment
+            Address address = new Address(args[0]);
+            GroupManager group = new GroupManager(address.getEmailsAddress(),groupNumber);
+
+            // Message managment
+            Message m = new Message(args[1]);
+            ArrayList<Mail> message = m.getMails();
+
+
+            SMTPClient client;
+            for(int i = 0; i < groupNumber ; ++i){
+                client = new SMTPClient(group.getSender(i),group.getReceiver(i),"localhost",1025 );
+                client.send(message.get(i).subject(),message.get(i).body());
+            }
+
+
+        }catch (Exception e){
+            System.out.println("An error has occured ! : " + e);
         }
 
-        Address addr = new Address(args[1]);
-
-        Message mess = new Message(args[2]);
-
-        Integer nbGroup = Integer.valueOf(args[3]);
-
-/*
-        ArrayList<String> mails = addr.getEmailsAddress();
-        ArrayList<Mail> messages = mess.getMails();
-
-        SMTPClient client = new SMTPClient(mails.get(0),mails,"localhost",1025 );
 
 
-        for(Mail m : messages){
-            client.send(m.subject(),m.body());
-        }
 
 
-*/
 
     }
 }
