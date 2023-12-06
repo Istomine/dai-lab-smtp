44eb333f1867371a934eb96662c63ac579c1c14a
Delay Benoit <benoit.delay@heig-vd.ch>
2023-12-06 11:54:56 +0100

    Added javadoc

--- smtp-client/src/main/java/com/dai/config/Mail.java
+++ smtp-client/src/main/java/com/dai/config/Mail.java
@@ -1,5 +1,10 @@
 package com.dai.config;
 
+/**
+ * Record to store an email who is caracterised by a subject and a body
+ * @param subject subject of a mail
+ * @param body body of the mail
+ */
 public record Mail(String subject, String body) {
 
 }
